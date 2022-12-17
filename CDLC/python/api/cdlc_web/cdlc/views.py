4a1c52c1c4de1605aafbaba2c6aaaf0718cfd657
MrPekar98 <mpch@cs.aau.dk>
2022-12-17 19:06:46 +0100

    Updated design using ChatGPT

--- CDLC/python/api/cdlc_web/cdlc/views.py
+++ CDLC/python/api/cdlc_web/cdlc/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.core.exceptions import BadRequest
 
 def index(request):
     template = loader.get_template('cdlc/index.html')
-    context = {
-        'test_message': 'Hello, World!'
-    }
+    context = {}
     return HttpResponse(template.render(context, request))
 
 # Make here an enpoint to handle POST request sent by the 'index' endpoint above for searching
+def result(request):
+    if request.method != 'POST':
+        raise BadRequest("Request must be POST")
+
+    use_embeddings = 'Embeddings' in request.POST.getlist('settings')
+    weighted_jaccard = 'Weighted_jaccard' in request.POST.getlist('settings')
+    cosine_function = request.POST.getlist('settings')[-1]
+    query = request.POST.getlist('query')[-1]
+
+    if len(query) == 0:
+        raise BadRequest("Missing query")
+
+    # Use connector to get result here
+
+    template = loader.get_template('cdlc/index.html')
+    context = {
+        'k': '100',
+        'result': ['T1']
+    }
+
+    return HttpResponse(template.render(context, request))
