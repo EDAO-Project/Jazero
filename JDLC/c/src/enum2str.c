38b54d0f1c1b0c13e296a2ed474b1e7f11b7cae8
MrPekar98 <martin.pekar@outlook.dk>
2023-05-03 13:44:34 +0200

    Prepare request

---
+++ JDLC/c/src/enum2str.c
@@ -0,0 +1,36 @@
+#include <jdlc.h>
+
+const char *c2str(enum cosine_function function)
+{
+    if (function == NORM_COS)
+    {
+        return "COSINE_NORM";
+    }
+
+    else if (function == ABS_COS)
+    {
+        return "COSINE_ABS";
+    }
+
+    return "COSINE_ANG";
+}
+
+const char *s2str(enum similarity_measure sim)
+{
+    if (sim == COSINE)
+    {
+        return "COSINE";
+    }
+
+    return "EUCLIDEAN";
+}
+
+const char *p2str(enum prefilter filter)
+{
+    if (filter == TYPES)
+    {
+        return "TYPES";
+    }
+
+    return "EMBEDDINGS";
+}
