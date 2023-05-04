618c4731e5dfaf3e873f88a574d9048a7dc699cf
MrPekar98 <martin.pekar@outlook.dk>
2023-05-04 16:20:23 +0200

    Finalized

--- JDLC/c/src/json_body.c
+++ JDLC/c/src/json_body.c
@@ -1,54 +1,27 @@
-// The caller of these functions must free the memory!
-
 #include <jdlc.h>
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 
-const char *load_embeddings_body(const char *file, const char *delimiter)
+const char *load_embeddings_body(char *buffer, const char *file, const char *delimiter)
 {
-    char *body = (char *) malloc(strlen(file) + strlen(delimiter) + 25);
-
-    if (body == NULL)
-    {
-        return NULL;
-    }
-
-    sprintf(body, "{\"file\": \"%s\", \"delimiter\": \"%s\"}", file, delimiter);
-    return body;
+    sprintf(buffer, "{\"file\": \"%s\", \"delimiter\": \"%s\"}", file, delimiter);
+    return buffer;
 }
 
-const char *load_body(const char *table_dir, const char *table_entity_prefix, const char *kg_prefix)
+const char *load_body(char *buffer, const char *table_dir, const char *table_entity_prefix, const char *kg_prefix)
 {
-    size_t size = strlen(table_dir) + strlen(table_entity_prefix) + strlen(kg_prefix) + 100;
-    char *body = (char *) malloc(size);
-
-    if (body == NULL)
-    {
-        return NULL;
-    }
-
-    sprintf(body, "{\"directory\": \"%s\", \"table-prefix\": \"%s\", \"kg-prefix\": \"%s\"}", table_dir, table_entity_prefix, kg_prefix);
-    return body;
+    sprintf(buffer, "{\"directory\": \"%s\", \"table-prefix\": \"%s\", \"kg-prefix\": \"%s\"}", table_dir, table_entity_prefix, kg_prefix);
+    return buffer;
 }
 
-/*const char *search_body(uint32_t top_k, uint8_t use_embeddings, enum cosine_function function,
+const char *search_body(char *buffer, uint32_t top_k, uint8_t use_embeddings, enum cosine_function function,
                         enum similarity_measure sim, enum prefilter lsh_prefilter, query q)
 {
     const char *cos_func_str = c2str(function), *sim_str = s2str(sim), *prefilter_str = p2str(lsh_prefilter);
     const char *query_str = q2str(q);
-    char *body = (char *) malloc(sizeof(char) * (strlen(cos_func_str) + strlen(sim_str) + strlen(prefilter_str) +
-            strlen(query_str) + 100));
-
-    if (body == NULL)
-    {
-        return NULL;
-    }
-
-    sprintf(body, "{\"top-k\": \"%d\", \"use_embeddings\": \"%s\", \"cosine-function\": \"%s\", "
+    sprintf(buffer, "{\"top-k\": \"%d\", \"use_embeddings\": \"%s\", \"cosine-function\": \"%s\", "
                   "\"single-column-per-query-entity\": \"true\", \"weighted-jaccard\": \"false\", "
                   "\"use-max-similarity-per-column\": \"true\", \"similarity-measure\": \"%s\", \"lsh\": \"%s\", "
                   "\"query\": \"%s\"}",
                   top_k, use_embeddings ? "true" : "false", cos_func_str, sim_str, prefilter_str, query_str);
-    return body;
-}*/
+    return buffer;
+}
--- JDLC/c/test/json_body_test.c
+++ JDLC/c/test/json_body_test.c
@@ -4,37 +4,71 @@
 
 int test_load_embeddings(void)
 {
-    const char *body = load_embeddings_body("some/file.txt", " ");
+    char *buffer = (char *) malloc(100);
+    load_embeddings_body(buffer, "some/file.txt", " ");
     const char *expected = "{\"file\": \"some/file.txt\", \"delimiter\": \" \"}";
 
-    if (strstr(body, expected) == NULL)
+    if (strstr(buffer, expected) == NULL)
     {
-        free((char *) body);
+        free(buffer);
         return 1;
     }
 
-    free((char *) body);
+    free(buffer);
     return 0;
 }
 
 int test_load(void)
 {
-    const char *body = load_body("some/dir/", "www", "http://www");
+    char *buffer = (char *) malloc(250);
+    load_body(buffer, "some/dir/", "www", "http://www");
     const char *expected = "{\"directory\": \"some/dir/\", \"table-prefix\": \"www\", \"kg-prefix\": \"http://www\"}";
 
-    //if (strcmp(body, expected) == 0)
-    if (strstr(body, expected) == NULL)
+    if (strstr(buffer, expected) == NULL)
     {
-        free((char *) body);
+        free(buffer);
         return 1;
     }
 
-    free((char *) body);
+    free(buffer);
     return 0;
 }
 
 int test_search(void)
 {
+    char ***rows = (char ***) malloc(sizeof(char **) * 2);
+    rows[0] = (char **) malloc(sizeof(char *) * 3);
+    rows[1] = (char **) malloc(sizeof(char *) * 3);
+    rows[0][0] = (char *) malloc(3);
+    rows[0][1] = (char *) malloc(3);
+    rows[0][2] = (char *) malloc(3);
+    rows[1][0] = (char *) malloc(3);
+    rows[1][1] = (char *) malloc(3);
+    rows[1][2] = (char *) malloc(3);
+    strcpy(rows[0][0], "e11");
+    strcpy(rows[0][1], "e12");
+    strcpy(rows[0][2], "e13");
+    strcpy(rows[1][0], "e21");
+    strcpy(rows[1][1], "e22");
+    strcpy(rows[1][2], "e23");
+
+    query q = make_query((const char ***) rows, 2, 3);
+    char *buffer = (char *) malloc(1000);
+    search_body(buffer, 10, 1, NORM_COS, EUCLIDEAN, EMBEDDINGS, q);
+    const char *expected = "{\"top-k\": \"10\", \"use_embeddings\": \"true\", \"cosine-function\": \"COSINE_NORM\", "
+                           "\"single-column-per-query-entity\": \"true\", \"weighted-jaccard\": \"false\", "
+                           "\"use-max-similarity-per-column\": \"true\", \"similarity-measure\": \"EUCLIDEAN\", "
+                           "\"lsh\": \"EMBEDDINGS\", \"query\": \"e11<>e12<>e13#e21<>e22<>e23\"}";
+
+    if (strstr(buffer, expected) == NULL)
+    {
+        free(buffer);
+        clear_query(q);
+        return 1;
+    }
+
+    free(buffer);
+    clear_query(q);
     return 0;
 }
 
